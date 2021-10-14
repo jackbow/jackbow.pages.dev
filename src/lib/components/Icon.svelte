34d4a507d18419e939cde4b7f7e6add414be2187
Jack Bowman <jackbow@umich.edu>
2021-10-13 19:31:38 -0600

    rm 3rd party scripts

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,14 +1,6 @@
 <script>
-	// export let name;
-	// export let width = "1.5rem";
-	// export let height = "1.5rem";
-	// export let focusable = false;
+	export let name;
 	import icons from '$lib/misc/icons.js';
-	console.log(icons);
 </script>
 
-{@html icons.heart}
-<!-- {@html icons[name]} -->
-<!-- <svg class={$$props.class} {focusable} {width} {height}>
-    <use href={`/icons.svg#${name}`} />
-</svg> -->
+{@html icons[name]}
