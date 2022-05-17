cc4295ac24b95cf1ab98f5d688b6794a6c5411b5
Jack Bowman <jackbow@umich.edu>
2022-05-16 20:42:56 -0400

    add toggle to jobs & about

--- content/about.js
+++ content/about.js
@@ -2,8 +2,8 @@ export default {
   paragraphs: [
     `Hello! I'm Jack, a software engineer based in Seattle, WA.`,
     `I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences.`,
-    `Shortly after graduating from the University of Michigan, I joined the engineering team at Mcity where I work on a wide variety of interesting and meaningful projects on a daily basis.`,
+    `Shortly after graduating from the University of Michigan and working at Mcity's autonomous vehicle testing facility engineering team, I joined the team at Toggle Industries where I work on creating a digital & robotic pipeline for rebar — one of the most difficult materials in construction.`,
     `Here are a few technologies I've been working with recently:`,
   ],
-  bullets: ["Vue", "Docker", "Python", "WebSockets", "PostgreSQL", "Cypress.io"],
+  bullets: ["React", "Typescript", "Go", "Docker", "gRPC", "PostgreSQL"],
 };
--- content/jobs.js
+++ content/jobs.js
@@ -1,15 +1,27 @@
 export default [
   {
     title: "Full-Stack Engineer",
+    company: "Toggle Industries",
+    location: "Remote",
+    range: "November 2021 - Present",
+    link: "https://toggle.build/",
+    bullets: [
+      `Building a complete software workflow and physical manufacturing system to transform one of the most difficult materials in construction into a digitally fabricated product.`,
+      `Architecting, designing, and developing systems directly driving the business via the intake and processing of architectural plans, 3D modeling of rebar assemblies, robotic motion planning, industrial machinery interfacing, and commerce & logistics.`,
+      `Second software hire, directly communicating with founders on a weekly basis, and collaborating in a team across 9 timezones.`,
+    ],
+  },
+  {
+    title: "Full-Stack Engineer",
     company: "Mcity",
     location: "Ann Arbor, MI",
     range: "March 2020 - Present",
     link: "https://www.mcity.umich.edu/",
     bullets: [
       `Developed research tools for the world’s largest connected & autonomous vehicle test environment in a small team.Used by Ford, GM, Toyota, Subaru, and BMW autonomous vehicle engineers among others.`,
-      `Created location based network visualization tool for connectivity, mapping, and searching of all facility devices.`,
+      `Created location based network visualization tool for connectivity, mapping, and searching of all facility devices 100x faster than previous workflow.`,
       `Added features, improved user interfaces, and triaged errors in existing applications.`,
-      `Implemented unit test and integration test suites hooked into CI / CD pipelines.`,
+      `Deployed unit test and integration test suites hooked into CI / CD pipelines.`,
       `On-boarded and trained a new-hire engineer.`,
     ],
   },
