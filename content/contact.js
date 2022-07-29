6e8b7111fd9291910044cb839ebaf91714185ebf
Jack Bowman <jackbow@umich.edu>
2022-07-28 23:03:22 -0400

    update: contact blurb

--- content/contact.js
+++ content/contact.js
@@ -1,3 +1,3 @@
 export const contactBlurb = `
-  I'm currently looking for new opportunities. Feel free to reach out for any reason, and I'll try my best to get back to you soon!
+  I'm always good for a chat. Feel free to reach out for any reason, and I'll try my best to get back to you soon!
 `;
