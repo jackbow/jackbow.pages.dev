b40ed9e5370881534ba3e5ef1fb693570139b998
Jack Bowman <jackbow@umich.edu>
2022-07-28 23:07:30 -0400

    feat: commit linter

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => /^build\(deps\): bump/.test(message)],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "cosmetic",
+        "docs",
+        "feat",
+        "fix",
+        "lint",
+        "migrate",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "ui",
+      ],
+    ],
+  },
+};
