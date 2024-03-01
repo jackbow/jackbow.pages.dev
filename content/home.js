9e2e9bdb30a414725f0c30e4bab82e33c7273ea2
Jack <jackbow@umich.edu>
2024-03-01 11:22:18 -0500

    chore: seattle -> detroit

--- content/home.js
+++ content/home.js
@@ -2,6 +2,6 @@ export const tinyBio = `
     I build things for the web.
   `;
 export const shortBio = `
-    I'm a software engineer based in Seattle, WA specializing in building (and occasionally designing)
+    I'm a software engineer based in Detroit, MI specializing in building (and occasionally designing)
     exceptional websites, applications, and everything in between.
   `;
