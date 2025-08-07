fa4e403dcacf564fe15c3f7f9bd1f1d1b9182081
Jack <15710512+jackbow@users.noreply.github.com>
2025-08-07 15:27:59 -0600

    feat: update resume

--- content/resume.js
+++ content/resume.js
@@ -1,1 +1,1 @@
-export const resumeUrl = "https://drive.google.com/file/d/1KqO_eInQj7wKK80BUsTAvhbrTxjWkjwm/view?usp=sharing";
+export const resumeUrl = "https://drive.google.com/file/d/1WIp7ROSmk95ZSGt-f3s6llsv7N_tEJ4I/view";
